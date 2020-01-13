1876288e3bc8b04c4da3a7422dd815c071ded0e1
Pirman Saputra <pirman0290@gmail.com>
2020-01-13 12:38:46 +0700

    Coba switch javascript

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-const nameA = 'Alpha'
-const nameB = 'Beta'
-const nameCompared = nameA === nameB
-
-console.log(nameA)
-console.log(nameB)
-console.log(nameCompared)
+const personA = {
+    name: 'Alpha',
+    age: 20
+  }
+  
+  const personB = {
+    name: 'Betty',
+    age: 30
+  }
+  
+  switch (personA.age > personB.age) {
+    case true:
+      console.log('Benar')
+      break
+  
+    case false:
+      console.log('Salah')
+      break
+  
+    default:
+      console.log('Seumuran')
+  }  
