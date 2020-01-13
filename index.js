844fc67dacb19ba94ac6df6d51394362b674b82c
Pirman Saputra <pirman0290@gmail.com>
2020-01-13 15:27:25 +0700

    Challange Code JS

--- index.js
+++ index.js
@@ -1,22 +1,57 @@
-const personA = {
-    name: 'Alpha',
-    age: 20
-  }
-  
-  const personB = {
-    name: 'Betty',
-    age: 30
-  }
-  
-  switch (personA.age > personB.age) {
-    case true:
-      console.log('Benar, Alpha lebih tua dari Betty')
-      break
-  
-    case false:
-      console.log('Salah, Alpha lebih muda dari Betty')
-      break
-  
-    default:
-      console.log('Seumuran')
-  }  
+const biodata = {
+  name: "Laila Ali",
+  age: 20,
+  collage: "University of Indonesia",
+  hobbies: ["swimming", "running", ["hiking", ["shopping", "traveling"]], "sleeping"],
+  address: {
+    street: "Kemang Raya",
+    province: "Jakarta Selatan",
+    country: "Indonesia"
+  },
+ contact: {
+   email: "laila@ali.com",
+   phone: {
+     countryCode: 021,
+     number: [13123,7968]
+   }
+ },
+ skills: {
+   language: ["English", "French", "Indonesia"],
+   programming: [{language: "JavaScript", framework: "React.js"}, "Java", "C++"]
+ }
+}
+
+// Write your code down below
+
+console.log(`Hai, my name is ${biodata.name}, I studied in ${biodata.collage}.`);
+
+console.log(`My programming skills are ${biodata.skills.programming[0].language} including using its framework ${biodata.skills.programming[0].framework}, ${biodata.skills.programming[1]} and ${biodata.skills.programming[2]}.`)
+
+console.log(`During school, I compete in many sports including ${biodata.hobbies[2][0]} and ${biodata.hobbies[1]}.`)
+
+console.log(`My hobbies are ${biodata.hobbies[3]} and also ${biodata.hobbies[2][1][0]}.`)
+
+console.log(`Right now I live at ${biodata.address.street}, ${biodata.address.province} - ${biodata.address.country}.`)
+
+console.log(`You can reach me on my email ${biodata.contact.email} or my phone number ${biodata.contact.phone.countryCode} - ${biodata.contact.phone.number[0]}${biodata.contact.phone.number[1]}.`)
+console.log('Thank you and have a nice day')
+
+
+
+//let score = prompt('Masukkan nilai komputer Anda')
+
+// Write your code down below
+//switch (true) {
+  //case score > 80:
+    //console.log("Excellent");
+    //break;
+  //case score >= 60 && score <= 80:
+    //console.log("Good Job");
+    //break;
+  //case score < 60:
+    //console.log("Don't give up");
+    //break;
+  //default:
+    //console.log("Undefined");
+    //break;
+//}
